554b8f7615171430036130543ac50437f6e7a927
unknown <oluwadamilolasandraodujirin@gmai.com>
2022-07-19 10:49:58 +0100

    setup node

---
+++ index.js
@@ -0,0 +1,9 @@
+const  express = require("express");
+const app = express();
+const mongoose =require("mongoose"); //connent to use mongoose
+const bodyParser = require("body-parser"); //connent to bodyparse
+const PORT = 8080;
+const userRouters = require("./routers/user"); 
+
+mongoose.connect("mongodb+srv://React:React@cluster0.83l2v.mongodb.net/?retryWrites=true&w=majority") 
+
